5f891ad5d7d7b2e7dc46c0af83dce2b676ccfbc0
MrCode-Man <minimina722@gmail.com>
2025-04-11 23:55:01 -0300

    a função 'insereDadosNo' foi feita com o objetivo de que, caso aumente o tanto de dados, ela facilitaria todo o processo (as funções de mudar idade e nome foram refatoradas para atender ao funcinamento dessa nova função). A função 'criaLista' cria na heap uma lista nova e retorna seu ponteiro. A função 'criaNo' ainda não está totalmente completa, estava muito de noite para eu terminar ela

--- hash table/src/funcoesLista.cpp
+++ hash table/src/funcoesLista.cpp
@@ -1,21 +1,68 @@
 #include "../include/funcoesLista.hpp" 
 
-void mudaNome(Dado& pessoa, std::string nome){
-
-        if(nome.empty()){
-                pessoa.nome = "default";
-                return;
-        }
-
-        pessoa.nome = nome;
+// funções para struct dado
+void insereDadosNo(unique_ptr<Dado>& dado){
+        
+        mudaNome(dado);
+        mudaIdade(dado);
 }
 
-void mudaIdade(Dado& pessoa, int idade){
 
-        if(idade <= 0){
-                pessoa.idade = 1;
-                return;
+void mudaNome(unique_ptr<Dado>& dado){
+        
+        std::cout << "informe o nome: ";
+        std::cin >> dado->nome;
+
+        std::cout << '\n';
+
+        if(dado->nome.empty() || dado->nome[0] == ' '){ 
+                dado->nome = "default";
         }
 
-        pessoa.idade = idade;
+}
+
+void mudaIdade(unique_ptr<Dado>& dado){
+        
+        std::cout << '\n' << "infome a idade: ";
+        std::cin >> dado->idade;
+
+        std::cout << '\n';
+
+        if(dado->idade <= 0){
+                dado->idade = 1;
+        }
+
+}
+
+
+// funções para struct No
+unique_ptr<No> criaNo(unique_ptr<Lista>& lista){
+
+        unique_ptr<No> novoNo = make_unique<No>();
+        unique_ptr<Dado> novoDado = make_unique<Dado>();
+
+        insereDadosNo(novoDado);
+
+        if(lista->comeco == nullptr){
+                novoNo->anterior = nullptr;
+                novoNo->proximo = nullptr;
+
+        } else {
+                // continuar depois                
+
+        }
+
+        return novoNo;
+}
+
+
+// funções para struct lista
+unique_ptr<Lista> criaLista(){
+
+        unique_ptr<Lista> lista = make_unique<Lista>();
+
+        lista->comeco = nullptr;
+        lista->final = nullptr;
+
+        return lista;
 }
