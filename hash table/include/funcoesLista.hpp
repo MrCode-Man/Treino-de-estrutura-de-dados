8fb6c435ea20aba354716dc4d37feda144390485
MrCode-Man <minimina722@gmail.com>
2025-04-11 23:50:49 -0300

    adicionado 3 novas funções: 'insereDadosNo', 'criaLista' e 'criaNo'

--- hash table/include/funcoesLista.hpp
+++ hash table/include/funcoesLista.hpp
@@ -1,6 +1,16 @@
 #pragma once
 
+#include <memory>
+#include <iostream>
+
 #include "../include/listaEncadeada.hpp"
 
-void mudaNome(Dado& pessoa, std::string nome);
-void mudaIdade(Dado& pessoa, int idade);
+using std::unique_ptr;
+using std::make_unique;
+
+void insereDadosNo(unique_ptr<Dado>& dado);
+void mudaNome(unique_ptr<Dado>& dado);
+void mudaIdade(unique_ptr<Dado>& dado);
+
+unique_ptr<Lista> criaLista();
+unique_ptr<No> criaNo(unique_ptr<Lista>& lista);
