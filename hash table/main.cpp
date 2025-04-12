41e435550417726593cd246e6b4c388f23293c2d
MrCode-Man <minimina722@gmail.com>
2025-04-11 23:42:25 -0300

    comecei os testes das funções e das structs queforam criadas

--- hash table/main.cpp
+++ hash table/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 
+#include "include/listaEncadeada.hpp"
+#include "src/funcoesLista.cpp"
+
+using std::cout;
+using std::cin;
+
 int main(){
 
+        // inicialização
+        Dado pessoa;
+        unique_ptr<Lista> lista = criaLista();        
+
+        // área de testes
         
+
+
         return 0;
 }
